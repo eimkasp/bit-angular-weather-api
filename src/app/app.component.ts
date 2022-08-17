0a5b9c83b3595da0def178b0e2f1a005e16d1cf7
Eimantas <eim.kasp@gmail.com>
2022-08-17 16:06:40 +0300

    Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bit-weather';
+  weatherData: any;
+  city : string = 'London';
+  apik = "3045dd712ffe6e702e3245525ac7fa38";
+
+
+  constuctor() {
+    console.log('this is construct');
+  }
+
+  ngOnInit() {
+    alert('hello');
+    this.getWeatherData('London');
+  }
+
+  getWeatherData(city: string) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      this.apik
+    )
+      .then((res) => res.json())
+
+      .then((data) => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+      })
+
+      .catch((err) => alert("You entered WRONG city name !!!"));
+  }
+
+  onChange($event : any) {
+    console.log('this is onChange');
+    console.log(this.city);
+    // I want to do something here with the new selectedDevice, but what I
+    // get here is always the last selection, not the one I just selected.
+  }
+
 }
